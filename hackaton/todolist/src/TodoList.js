da39b35aeebb171f5cb3caaed90f53c56eb568da
zygoh1986 <69612872+zygoh1986@users.noreply.github.com>
2021-01-02 17:35:32 +0800

    TodoList

---
+++ hackaton/todolist/src/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = ({todos, setTodos}) => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <Todo setTodos={setTodos} todos={todos} key={todo.id} text={todo.text}/>
+
+                ))}
+            </ul>
+            
+        </div>
+    );
+};
+
+export default TodoList;
